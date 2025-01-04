39d1fe297107d0a9afd488150a8a82ec46c7b886
Victor Kamau <victormungai97@gmail.com>
2025-01-04 23:48:33 +0300

    feat: :tada: Initial Commit
    Began development on test wellness tracker application for Sundial Lands

---
+++ android/app/src/main/java/sundial/wellness/tracker/android/apps/sundial_wellness_tracker/MainActivity.java
@@ -0,0 +1,6 @@
+package sundial.wellness.tracker.android.apps.sundial_wellness_tracker;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
