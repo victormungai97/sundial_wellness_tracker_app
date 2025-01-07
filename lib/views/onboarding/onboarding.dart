b63c1ab0bc66982eabb55a2721a42877150d4642
Victor Kamau <victormungai97@gmail.com>
2025-01-07 20:59:59 +0300

    perf: :zap: Onboarding
    Improved functionality, and hence performance, around retrieval of motivational message from server during onboarding

--- lib/views/onboarding/onboarding.dart
+++ lib/views/onboarding/onboarding.dart
@@ -7,6 +7,7 @@ import 'package:lottie/lottie.dart';
 import 'package:sundial_wellness_tracker/bloc/cubits/motivation/motivation_cubit.dart';
 import 'package:sundial_wellness_tracker/bloc/cubits/onboarding/onboarding_cubit.dart';
 import 'package:sundial_wellness_tracker/navigation/navigation.dart';
+import 'package:sundial_wellness_tracker/widgets/stateful_wrapper.dart';
 
 class OnboardingPage extends HookWidget {
   const OnboardingPage({super.key});
@@ -36,130 +37,129 @@ class OnboardingPage extends HookWidget {
   Widget build(BuildContext context) {
     final motivationCubit = context.watch<MotivationCubit>();
 
-    return SafeArea(
-      child: IntroductionScreen(
-        pages: [
-          PageViewModel(
-            title: 'Journal',
-            body: 'Note down all your thoughts and emotions',
-            image: Padding(
-              padding: const EdgeInsets.only(top: 24),
-              child: Center(
-                child: Lottie.asset(
-                  'assets/animations/journaling_animation.json',
-                  height: double.infinity,
-                  width: double.infinity,
+    return StatefulWrapper(
+      onInit: motivationCubit.retrieveRandom,
+      child: SafeArea(
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: 'Journal',
+              body: 'Note down all your thoughts and emotions',
+              image: Padding(
+                padding: const EdgeInsets.only(top: 24),
+                child: Center(
+                  child: Lottie.asset(
+                    'assets/animations/journaling_animation.json',
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
                 ),
               ),
+              decoration: _pageDecoration,
             ),
-            decoration: _pageDecoration,
-          ),
-          PageViewModel(
-            title: 'Dashboard',
-            body: 'View your mood and health metrics',
-            image: Lottie.asset(
-              'assets/animations/dashboard_animation.json',
-              height: double.infinity,
-              width: double.infinity,
+            PageViewModel(
+              title: 'Dashboard',
+              body: 'View your mood and health metrics',
+              image: Lottie.asset(
+                'assets/animations/dashboard_animation.json',
+                height: double.infinity,
+                width: double.infinity,
+              ),
+              decoration: _pageDecoration,
             ),
-            decoration: _pageDecoration,
-          ),
-          PageViewModel(
-            titleWidget: BlocBuilder<MotivationCubit, MotivationState>(
-              builder: (context, state) {
-                final text = state is MotivationRandomRetrievalState
-                    ? state.whenOrNull(
-                        success: (motivation) => motivation.message,
-                      )
-                    : null;
-                return Text(
-                  text != null ? '"$text"' : "Let's get you started",
-                  style: text == null
-                      ? _pageDecoration.titleTextStyle
-                      : _pageDecoration.bodyTextStyle,
-                  textAlign: TextAlign.center,
-                );
-              },
-            ),
-            bodyWidget: BlocBuilder<MotivationCubit, MotivationState>(
-              builder: (context, state) {
-                final text = state is MotivationRandomRetrievalState
-                    ? state.whenOrNull(
-                        success: (motivation) => motivation.author,
-                      )
-                    : null;
-                final messageAvailable = text == null;
-                return Column(
-                  children: [
-                    Text(
-                      text ?? "You're doing great!\nKeep it up!",
-                      style: !messageAvailable
-                          ? _pageDecoration.titleTextStyle
-                          : _pageDecoration.bodyTextStyle,
-                      textAlign:
-                          messageAvailable ? TextAlign.center : TextAlign.end,
-                    ),
-                    const SizedBox(height: 12),
-                    SizedBox(
-                      width: 250,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          context.read<OnboardingCubit>().onBoardingDone();
-                          context.go(Routes.journal);
-                        },
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.purple,
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(6)),
-                          ),
-                          textStyle: const TextStyle(
-                            fontFamily: 'Nunito',
-                            fontSize: 16,
-                          ),
-                        ),
-                        child: const Text('Start Journaling'),
+            PageViewModel(
+              titleWidget: BlocBuilder<MotivationCubit, MotivationState>(
+                builder: (context, state) {
+                  final text = state is MotivationRandomRetrievalState
+                      ? state.whenOrNull(
+                          success: (motivation) => motivation.message,
+                        )
+                      : null;
+                  return Text(
+                    text != null ? '"$text"' : "Let's get you started",
+                    style: text == null
+                        ? _pageDecoration.titleTextStyle
+                        : _pageDecoration.bodyTextStyle,
+                    textAlign: TextAlign.center,
+                  );
+                },
+              ),
+              bodyWidget: BlocBuilder<MotivationCubit, MotivationState>(
+                builder: (context, state) {
+                  final text = state is MotivationRandomRetrievalState
+                      ? state.whenOrNull(
+                          success: (motivation) => motivation.author,
+                        )
+                      : null;
+                  final messageAvailable = text == null;
+                  return Column(
+                    children: [
+                      Text(
+                        text ?? "You're doing great!\nKeep it up!",
+                        style: !messageAvailable
+                            ? _pageDecoration.titleTextStyle
+                            : _pageDecoration.bodyTextStyle,
+                        textAlign:
+                            messageAvailable ? TextAlign.center : TextAlign.end,
                       ),
-                    ),
-                  ],
-                );
-              },
+                      const SizedBox(height: 12),
+                      SizedBox(
+                        width: 250,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            context.read<OnboardingCubit>().onBoardingDone();
+                            motivationCubit.reset();
+                            context.go(Routes.journal);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.purple,
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(6),
+                              ),
+                            ),
+                            textStyle: const TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 16,
+                            ),
+                          ),
+                          child: const Text('Start Journaling'),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              image: Lottie.asset(
+                'assets/animations/get_started_animation.json',
+                height: double.infinity,
+                width: double.infinity,
+              ),
+              decoration: _pageDecoration,
             ),
-            image: Lottie.asset(
-              'assets/animations/get_started_animation.json',
-              height: double.infinity,
-              width: double.infinity,
+          ],
+          showDoneButton: false,
+          showSkipButton: true,
+          skip: const Text(
+            'Skip',
+            style: TextStyle(
+              fontFamily: 'Nunito',
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
             ),
-            decoration: _pageDecoration,
           ),
-        ],
-        onChange: (value) async {
-          if (value == 2) {
-            await motivationCubit.retrieveRandom();
-          } else {
-            motivationCubit.reset();
-          }
-        },
-        showDoneButton: false,
-        showSkipButton: true,
-        skip: const Text(
-          'Skip',
-          style: TextStyle(
-            fontFamily: 'Nunito',
-            fontWeight: FontWeight.w600,
-            fontSize: 18,
+          dotsDecorator: const DotsDecorator(
+            size: Size(10, 10),
+            color: Color(0xFFBDBDBD),
+            activeSize: Size(22, 10),
+            activeShape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(25)),
+            ),
           ),
+          curve: Curves.fastLinearToSlowEaseIn,
+          next: const Icon(Icons.arrow_forward, color: Colors.purple, size: 24),
         ),
-        dotsDecorator: const DotsDecorator(
-          size: Size(10, 10),
-          color: Color(0xFFBDBDBD),
-          activeSize: Size(22, 10),
-          activeShape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(25)),
-          ),
-        ),
-        curve: Curves.fastLinearToSlowEaseIn,
-        next: const Icon(Icons.arrow_forward, color: Colors.purple, size: 24),
       ),
     );
   }
